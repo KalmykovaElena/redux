e9a3995faa050b0a19a59b1b1ee5fd415d1ac6fb
KalmykovaElena <kalmykhovae@gmail.com>
2023-01-03 18:10:01 +0300

    home work

--- src/components/store-header/store-header.jsx
+++ src/components/store-header/store-header.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import './store-header.css'
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
 
-const StoreHeader = () => {
+const StoreHeader = ({cartItems}) => {
+
+    const itemsInCartCount = cartItems.length;
+    const cartTotal = cartItems.reduce((acc,item)=>{
+       return acc+=item.total
+    },0)
+    const cartTotalCount = cartItems.reduce((acc,item)=>{
+       return acc+=item.count
+    },0)
+
     return (
         <header className={'store-header'}>
             <Link className="logo text-dark" to='/'>
                 Shop
             </Link>
+            <span>Cart total:{cartTotal}</span>
             <Link className="shopping-cart" to='/cart'>
-                <i className={'cart-icon bi bi-cart-fill'}/>5 items
+                <i className={'cart-icon bi bi-cart-fill'} /><span>{itemsInCartCount} items</span>
+                <div>{cartTotalCount} : total count</div>
             </Link>
         </header>
     );
 };
+const mapStateToProps = (state) => {
+    return {
+        cartItems: state.cartItems
+    }
 
-export default StoreHeader;
+}
+
+export default  connect(mapStateToProps)(StoreHeader);
